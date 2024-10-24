8ad8fa33ef7bf2467d61d664f0156e4845e339e4
JuanRG-20221039 <20221039@uthh.edu.mx>
2024-10-24 00:43:05 -0600

    modified:   src/user/pages/UserLayout.jsx 	modified:   src/admin/pages/users/AddUser.jsx 	modified:   src/auth/pages/Login.jsx 	modified:   src/auth/pages/Registro.jsx 	modified:   src/main.jsx

--- src/user/pages/UserLayout.jsx
+++ src/user/pages/UserLayout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Header from '../../components/Header'
+import UserProfile from './UserProfile'
 
 export default function UserLayout() {
   return (
     <div>
       <Header/>
-      <div>USER LAYOUT</div>
+      <h1>USER LAYOUT</h1>
+      <UserProfile/>
     </div>
   )
 }
