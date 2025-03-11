c256c0b90c5406a15bab946dc1a331ec05c51e3a
20221039@uthh.edu.mx <20221039@uthh.edu.mx>
2025-03-10 23:30:53 -0600

    modified:   src/admin/pages/contextoContemporaneo/ContextoContemporaneoAdmin.jsx

--- src/admin/pages/contextoContemporaneo/ContextoContemporaneoAdmin.jsx
+++ src/admin/pages/contextoContemporaneo/ContextoContemporaneoAdmin.jsx
@@ -575,6 +575,8 @@ export default function ContextoContemporaneoAdmin() {
           <Spinner />
         )}
 
+        <br></br>
+        <hr></hr>
         <div className="mt-8">
           <h3 className="text-2xl font-bold mb-4">Lista de PDFs Enviados</h3>
           {pdfList.length === 0 ? (
@@ -608,7 +610,7 @@ export default function ContextoContemporaneoAdmin() {
           </ul>          
           )}
         </div>
-        
+
       </form>
     </div>
   );
